7c26df7d83cfded878451e54306408c84718febe
Elhasnaoui soufiane <soufiane.elhasnaoui.01@gmail.com>
2024-06-27 20:44:28 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -48,5 +48,5 @@ FinalData <- TidyData %>%
         group_by(subject, activity) %>%
         summarise_all(funs(mean))
 
-# Step 8: Save the tidy data set to a file
+# Saving the tidy data set to a file
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
